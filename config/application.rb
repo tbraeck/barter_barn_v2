7ccbf2b16e563780ed3443ccd3fe3a55d6f49e0a
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-01-25 05:14:49 -0700

    fixing postgres problems

--- config/application.rb
+++ config/application.rb
@@ -11,14 +11,12 @@ module BarterBarnV2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
    
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
-    config.action_dispatch.cookies_same_site_protection = :strict
+   
     config.load_defaults 7.1
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Configuration for the application, engines, and railties goes here.
     #
